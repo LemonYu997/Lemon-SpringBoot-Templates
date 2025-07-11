199443a27c8d03344da7c5891d8cf4e62c3ea6de
钰见 <1445531395@qq.com>
2025-07-11 15:38:35 +0800

    add:SaToken-demo

--- SpringBoot-SaToken/src/main/java/app/config/SaTokenConfigure.java
+++ SpringBoot-SaToken/src/main/java/app/config/SaTokenConfigure.java
@@ -1,6 +1,8 @@
 package app.config;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +13,54 @@ public class SaTokenConfigure implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册 Sa-Token 拦截器，打开注解式鉴权功能
-        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+//        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+
+        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。排除了/user/doLogin接口用来开放登录
+//        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/user/doLogin");
+
+        // 注册 Sa-Token 拦截器，定义详细认证规则
+//        registry.addInterceptor(new SaInterceptor(handler -> {
+//            // 指定一条 match 规则
+//            SaRouter
+//                    .match("/**")    // 拦截的 path 列表，可以写多个 */
+//                    .notMatch("/user/doLogin")        // 排除掉的 path 列表，可以写多个
+//                    .check(r -> StpUtil.checkLogin());        // 要执行的校验动作，可以写完整的 lambda 表达式
+//
+//            // 根据路由划分模块，不同模块不同鉴权
+//            SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
+//            SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+//            SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+//            SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+//            SaRouter.match("/notice/**", r -> StpUtil.checkPermission("notice"));
+//            SaRouter.match("/comment/**", r -> StpUtil.checkPermission("comment"));
+//        })).addPathPatterns("/**");
+
+        // 注册路由拦截器，自定义认证规则
+        registry.addInterceptor(new SaInterceptor(handler -> {
+
+            // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
+            SaRouter.match("/**", "/user/doLogin", r -> StpUtil.checkLogin());
+
+            // 角色校验 -- 拦截以 admin 开头的路由，必须具备 admin 角色或者 super-admin 角色才可以通过认证
+            SaRouter.match("/admin/**", r -> StpUtil.checkRoleOr("admin", "super-admin"));
+
+            // 权限校验 -- 不同模块校验不同权限
+            SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
+            SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+            SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+            SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+            SaRouter.match("/notice/**", r -> StpUtil.checkPermission("notice"));
+            SaRouter.match("/comment/**", r -> StpUtil.checkPermission("comment"));
+
+            // 甚至你可以随意的写一个打印语句
+            SaRouter.match("/**", r -> System.out.println("----啦啦啦----"));
+
+            // 连缀写法
+            SaRouter.match("/**").check(r -> System.out.println("----啦啦啦----"));
+
+        })).addPathPatterns("/**");
+
     }
 }
--- SpringBoot-SaToken/src/main/java/app/controller/UserController.java
+++ SpringBoot-SaToken/src/main/java/app/controller/UserController.java
@@ -1,5 +1,6 @@
 package app.controller;
 
+import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,5 +48,16 @@ public class UserController {
     public SaResult logout() {
         StpUtil.logout();
         return SaResult.ok();
+    }
+
+    // 登录接口 后端将 token 返回到前端 ，无 Cookie模式
+    @RequestMapping("/login")
+    public SaResult doLogin() {
+        // 第1步，先登录上
+        StpUtil.login(10001);
+        // 第2步，获取 Token  相关参数
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+        // 第3步，返回给前端
+        return SaResult.data(tokenInfo);
     }
 }
